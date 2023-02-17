75157cd9c86dc0a3159a3926bf489b66fbac5d4e
Arnav Patel <patelarnavm+github@gmail.com>
2023-02-17 14:16:24 -0500

    firestore is working now

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -24,22 +24,37 @@ function SignUp() {
 
       const uploadTask = uploadBytesResumable(storageRef, avatar);
 
+      // Register three observers:
       uploadTask.on(
         "state_changed",
-        (snapshot) => {},
-        (error) => {},
+        // (snapshot) => {
+        //   const progress =
+        //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //   console.log("Upload is " + progress + "% done");
+        //   switch (snapshot.state) {
+        //     case "paused":
+        //       console.log("Upload is paused");
+        //       break;
+        //     case "running":
+        //       console.log("Upload is running");
+        //       break;
+        //   }
+        // },
+        // (error) => {
+        // setErr(err);
+        // },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            console.log("File available at", downloadURL);
             await updateProfile(res.user, {
               displayName: username,
               photoURL: downloadURL,
             });
+
             await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
               displayName: username,
               email,
               photoURL: downloadURL,
-              uid: res.user.uid,
             });
           });
         }
